a492548e19581339a582595ba9b0d277cabb3788
d3mola <ariyaademola@gmail.com>
2017-10-17 00:29:27 +0100

    Serve  index.html file to the browser

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send("Welcome to my app");
+  //res.send("Welcome to my app");
+  res.sendFile(`${__dirname}/index.html`)
+  console.log('worked');
 })
 
 app.listen(port, () => {
