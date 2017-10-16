88590053e730c50dd675a620ef23b4b2fd1bdb1d
d3mola <ariyaademola@gmail.com>
2017-10-16 23:30:30 +0100

    Setup express app and start server

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+// create/ set-up an express app
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send("Welcome to my app");
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}...`)
+})
