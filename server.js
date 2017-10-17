3ca85503b196d88f16a7f2a030841263b2cd2aa9
d3mola <ariyaademola@gmail.com>
2017-10-17 02:08:52 +0100

    Add body-parser to parse request bodies

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 import express from 'express';
-
+import bodyParser from 'body-parser';
 // create/ set-up an express app
 const app = express();
 const port = 3000;
 
+// Parse body of incoming requests
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
   //res.send("Welcome to my app");
-  res.sendFile(`${__dirname}/index.html`)
-  console.log('worked');
+  res.sendFile(`${__dirname}/index.html`);
+  console.log('Working as expected!');
 })
 
 app.listen(port, () => {
