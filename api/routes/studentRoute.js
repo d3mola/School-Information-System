b46a4cc092d1df03e80399e24ee5de8ac0b39c3c
d3mola <ariyaademola@gmail.com>
2017-10-18 13:05:47 +0100

    Refactor code to reflect   airbnb standard

--- api/routes/studentRoute.js
+++ api/routes/studentRoute.js
@@ -1,25 +1,24 @@
-module.exports = function(app) {
+module.exports = function (app) {
   const studentController = require('../controllers/studentController');
 
   // student routes
   app.route('/api/students')
-  .get(studentController.list_all_students)
-  .post(studentController.create_a_student)
-  //.delete(studentController.delete_a_student)
-  //.put(studentController.update_a_student)
+    .get(studentController.listAllStudents)
+    .post(studentController.createAStudent);
+  // .delete(studentController.delete_a_student)
+  // .put(studentController.update_a_student)
 
 
   app.route('/api/students/:studentId')
-  //.get(studentController.list_a_student)
-  .put(studentController.update_a_student_info)
-  .delete(studentController.delete_a_student)
-
+  // .get(studentController.list_a_student)
+    .put(studentController.updateAStudentInfo)
+    .delete(studentController.deleteAStudent);
 };
 
 
 /**
  * es6 syntax.. have problems with importing and exporting
- * 
+ *
 import { list_all_students, create_a_student, update_a_student, delete_a_student } from '../controllers/studentController';
 
 const studentController = app => {
