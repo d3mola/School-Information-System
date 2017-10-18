993b67eaf1418e5ebb391755945b397c3d4b5ca5
d3mola <ariyaademola@gmail.com>
2017-10-18 12:07:00 +0100

    Refactor code to use proper style guide

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import Student from './api/models/studentModel.js'; // created model loading here
-import routes from './api/routes/studentRoute.js'; // importing route
+// import Student from './api/models/studentModel'; // created model loading here
+import routes from './api/routes/studentRoute'; // importing route
 
 // Create/ Set-up an express app
 const app = express();
@@ -13,21 +13,21 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://demola:demola2@ds119355.mlab.com:19355/school-info-db');
 
 // Parse body of incoming requests
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
 // Routes go here
 // test route
 app.get('/', (req, res) => {
-  res.send("Welcome to my app");
-  //res.sendFile(`${__dirname}/index.html`);
+  res.send('Welcome to my app');
+  // res.sendFile(`${__dirname}/index.html`);
   console.log('Working as expected!');
-})
+});
 
 // imported routes registered here
 routes(app);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}...`)
-})
+  console.log(`Server Listening on port ${port}...`);
+});
