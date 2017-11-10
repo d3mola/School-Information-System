a92fbe112c0b9c4e428d43d7e739e27dcbaa362a
d3mola <ariyaademola@gmail.com>
2017-11-11 00:47:38 +0100

    Add controller for forms to add and update a student; change mongoose method to remove student

--- api/controllers/studentController.js
+++ api/controllers/studentController.js
@@ -16,6 +16,12 @@ const listAllStudents = (req, res) => {
   });
 };
 
+/**
+ * Get a single student with studentId params
+ * @param {obj} req
+ * @param {obj} res
+ * @returns {student} one student
+ */
 const getAStudent = (req, res) => {
   Student.findById({ _id: req.params.studentId }, (err, student) => {
     if (err) {
@@ -23,6 +29,13 @@ const getAStudent = (req, res) => {
     }
     // res.json(student);
     res.render('onestudent.pug', { title: 'School Management System', student });
+  });
+};
+
+// form to add a student
+const addStudentForm = (req, res) => {
+  res.render('addstudent', {
+    title: 'School Management System'
   });
 };
 
@@ -39,30 +52,44 @@ const createAStudent = (req, res) => {
       res.send(err.toString());
     }
     console.log(student);
-    res.redirect('/api/students');
+    res.redirect('/api/student/add');
     // res.json(student);
   });
 };
 
+// form to update a student
+const updateStudentForm = (req, res) => {
+  Student.findById({ _id: req.params.studentId }, (err, student) => {
+    if (err) {
+      res.send(err.toString());
+    }
+    // res.json(student);
+    res.render('update_student.pug', { title: 'School Management System', student });
+  });
+};
+
+// update student controller
 const updateAStudentInfo = (req, res) => {
   Student.findOneAndUpdate({ _id: req.params.studentId }, req.body, { new: true }, (err, student) => {
     if (err) {
-      res.send(err);
+      res.send(err.toString());
     }
-    res.json(student);
+    // res.json(student);
+    // console.log(student);
+    res.redirect(`/api/students/${student._id}`);
   });
 };
 
+// controller to delete a student
 const deleteAStudent = (req, res) => {
-  Student.remove({ _id: req.params.studentId }, (err, student) => {
+  Student.findByIdAndRemove({ _id: req.params.studentId }, (err, student) => {
     if (err) {
       res.send(err);
     }
-    res.json({
-      message: 'Student succesfully deleted',
-      student
-    });
+    console.log(student);
+    // res.json({ message: 'Student succesfully deleted', student });
+    res.redirect('/api/students');
   });
 };
 
-export { listAllStudents, getAStudent, createAStudent, updateAStudentInfo, deleteAStudent };
+export { listAllStudents, addStudentForm, updateStudentForm, getAStudent, createAStudent, updateAStudentInfo, deleteAStudent };
